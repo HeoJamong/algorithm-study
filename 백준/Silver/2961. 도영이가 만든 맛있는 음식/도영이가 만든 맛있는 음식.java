3bc875a9e478735cd0f4547458c5c22c1a27b4ed
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-03-24 22:59:56 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,42 +1,36 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  public static int N;
-  static long result = Integer.MAX_VALUE;
-  public static int[] S, B;
+	static int[][] select;
+	static int N;
+	static long result = Long.MAX_VALUE;
 
-  private static void fun(int cnt, long sTotal, long bTotal, int count) {
-    if (cnt == N) {
-      if (count > 0) {
-        result = Math.min(result, Math.abs(sTotal - bTotal));
-      }
-      return;
-    }
-    fun(cnt + 1, sTotal * S[cnt], bTotal + B[cnt], count + 1);
-
-    fun(cnt + 1, sTotal, bTotal, count);
-
-  }
-
-  public static void main(String[] args) throws NumberFormatException, IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-
-    N = Integer.parseInt(br.readLine());
-
-    S = new int[N];
-    B = new int[N];
-
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      S[i] = Integer.parseInt(st.nextToken());
-      B[i] = Integer.parseInt(st.nextToken());
-    }
-
-    fun(0, 1, 0, 0);
-    System.out.println(result);
-  }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		select = new int[N][2];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			select[i][0] = Integer.parseInt(st.nextToken());
+			select[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 1; i < (1 << N); i++) {
+			int sour = 1;
+			int bitter = 0;
+			for (int k = 0; k < N; k++) {
+				if ((i & (1 << k)) != 0) {
+					sour *= select[k][0];
+					bitter += select[k][1];
+				}
+			}
+			result = Math.min(result, Math.abs(sour - bitter));
+		}
+		System.out.println(result);
+	}
 }
