13f452062ff0e37795b2d9d3fbb9dd01577a7a60
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-04-16 01:47:52 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 42212 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,35 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		String[] arr = new String[n];
-		
-		for (int i = 0; i < n; i++) {
-			arr[i] = br.readLine();
-		}
-		arr = Arrays.stream(arr).distinct().toArray(String[]::new);
-        
-		Arrays.sort(arr, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				if (o1.length() != o2.length()) {
-					return o1.length() - o2.length();
-				}
-				return o1.compareTo(o2);
-			}
-		});
-		
-		for (String s : arr) {
-			System.out.println(s);
-		}
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        TreeSet<String> set = new TreeSet<>((a, b) -> {
+            int size = a.length() - b.length();
+            if (size == 0) {
+                return a.compareTo(b);
+            }
+            return size;
+        });
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        for (String s : set) {
+            sb.append(s + "\n");
+        }
+        System.out.print(sb);
+    }
 }
