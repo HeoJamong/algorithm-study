636e9b25946bf44555131fd3a5361be8a561d08d
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-04-24 01:59:43 +0900

    [level 2] Title: 더 맵게, Time: 1627.82 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int n : scoville) {
+            pq.add(n);
+        }
+        
+        int cnt = 0;
+        while (true) {
+            if (pq.peek() >= K) return cnt;
+            if (pq.size() == 1) break;
+            
+            int fir = pq.poll();
+            int sec = pq.poll();
+            
+            // System.out.println("fir:" + fir);
+            // System.out.println("sec:" + sec);
+            // System.out.println("new:" + (fir + (sec * 2)));
+            pq.add(fir + (sec * 2));
+            cnt++;
+        }
+        
+        
+        return -1;
+    }
+}
