bec3e67ca089168ab6d9a1e63165b27f6467a683
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-04-24 01:22:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.74 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 97 MB, 시간: 87.03 ms
+메모리: 97 MB, 시간: 65.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 09:45:37
+2025년 04월 24일 01:22:57
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        // 참가자를 map에 넣고 동명이인일 경우 값을 1 증가
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
         }
         
-        for (String c : completion) {
-            map.put(c, map.get(c) -1);
+        // 이름을 key로 해서 탐색하여 값을 1씩 줄임
+        // 값이 0이 되면 remove?
+        
+        for (String s : completion) {
+            map.put(s, map.get(s) - 1);
         }
         
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                return key;
+        for (String s : participant) {
+            if (map.get(s) != 0) {
+                return s;
             }
         }
-        return answer;
+        
+        return null;
     }
 }
