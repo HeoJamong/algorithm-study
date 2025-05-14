49c0ee3e9e6189a37a75225305d55d45bdfa470c
HeoJamong <84281455+HeoJamong@users.noreply.github.com>
2025-05-14 22:22:10 +0900

    [level 3] Title: 섬 연결하기, Time: 0.75 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,40 +1,44 @@
 import java.util.*;
 
 class Solution {
+
     static int[] parent;
     
     public int find(int x) {
-        if (parent[x] == x) return x;
+        if (parent[x] == x) {
+            return x;
+        }
         return parent[x] = find(parent[x]);
     }
     
     public void union(int a, int b) {
-        int pa =find(a);
+        int pa = find(a);
         int pb = find(b);
         
-        if (pa != pb) parent[pa] = pb;
+        if (pa != pb) {
+            parent[pa] = pb;
+        }
     }
     
+    
     public int solution(int n, int[][] costs) {
-        
         parent = new int[n];
         
-        for (int i =0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
         Arrays.sort(costs, (a, b) -> a[2] - b[2]);
         
-        int total  = 0;
+        int total = 0;
         int edgeCount = 0;
         
-        for (int[] edge: costs) {
+        for (int[] edge : costs) {
             int a = edge[0];
             int b = edge[1];
-            int cost = edge[2];
+            int cost= edge[2];
             
-            // a와 b의 부모가 다름 -> 사이클 아님
-            if (find(a) != find (b)) { 
+            if (find(a) != find(b)) {
                 union(a, b);
                 total += cost;
                 edgeCount++;
@@ -42,8 +46,10 @@ class Solution {
                 if (edgeCount == n - 1) {
                     break;
                 }
-            }                
-        }  
+            }
+        }
+        
         return total;
+        
     }
 }
